028f9195010c22dc4fcc37367c10202769b80a08
motoyama1020 <babywalk18@gmail.com>
2020-12-02 19:21:25 +0900

    ユーザー情報追加、送信フォーム編集

--- db/migrate/20201124025429_devise_create_users.rb
+++ db/migrate/20201124025429_devise_create_users.rb
@@ -7,6 +7,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.integer :event_time
+      t.integer :clear_time
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 2020_11_27_025451) do
     t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.integer "event_time"
+    t.integer "clear_time"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
