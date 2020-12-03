a235ef3ca4eec0f93d3902a3957fc247171d017f
motoyama1020 <babywalk18@gmail.com>
2020-12-03 17:45:57 +0900

    Revert "検索フォーム修正"
    This reverts commit 39e98a35ebd7e565a91c0156817028d9c4701f50.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,11 +1,8 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :search]
-  before_action :search_room, only: [:index, :search]
 
   def index
     @rooms = Room.all.order(created_at: :desc)
-    set_product_column
-    set_category_column
   end
 
   def new
@@ -30,24 +27,12 @@ class RoomsController < ApplicationController
   
   def search
     @rooms = Room.search(params[:keyword]).order(created_at: :desc)
-    @results = @p.result.includes(:prefecture)
   end
 
   private
 
   def room_params
     params.require(:room).permit(:year_id, :month_id, :capacity_id, :prefecture_id,:name, user_ids: [])
-  end
-
-  def search_room
-    @p = Room.ransack(params[:q])
-  end
-
-  def set_room_column
-    @room_prefecture = Room.select("prefecture").distinct
-    @room_year = Room.select("year").distinct
-    @room_month = Room.select("month").distinct
-    @room_capacity = Room.select("capacity").distinct
   end
 
 end
