f4634d8acc10a08bbde42927ccd7c6e738591407
motoyama1020 <babywalk18@gmail.com>
2020-12-10 16:23:51 +0900

    Revert "Revert "パスワードも編集できるようにした""
    This reverts commit 68eb2b335bfb6d70fe5da63b9a2a8d7669c0ffd7.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, unless: :was_attached?
+  validates :name, presence: true
+  
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, on: :new
+  validates_format_of :password, with: PASSWORD_REGEX
 
   with_options format: { with: /\A[0-9]+\z/ }, on: :update, allow_nil: true do
     validates :event_time, numericality: { greater_than_or_equal_to: 1 }
