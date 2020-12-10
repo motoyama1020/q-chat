d0d394e73294cd1e0c113bf3dd5db6a7b460e846
motoyama1020 <babywalk18@gmail.com>
2020-12-10 16:21:44 +0900

    Revert "Revert "ユーザーアイコン表示""
    This reverts commit fc5be42cd0f6e3099392a1c182a3b87ce4d2a1cf.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,20 +4,19 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
-    @rooms = Room.includes(:users,:rooms).order(created_at: :desc)
-    @messages = @room.messages.includes(:user)
+    @rooms = Room.includes(:users,:rooms, { image_attachment: :blob }).order(created_at: :desc)
+    @messages = @room.messages.includes(:user, { image_attachment: :blob })
   end
 
   def create
     @room = Room.find(params[:room_id])
-
     @message = @room.messages.new(message_params)
     if @message.valid?
       @message.save
       redirect_to room_messages_path(@room)
     else
-      @rooms = Room.includes(:users,:rooms).order(created_at: :desc)
-      @messages = @room.messages.includes(:user)
+      @rooms = Room.includes(:users,:rooms, { image_attachment: :blob }).order(created_at: :desc)
+      @messages = @room.messages.includes(:user, { image_attachment: :blob })
       render :index
     end
   end
