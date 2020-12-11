d13eaeb10c0a591befcb25c41dc6ca9427d23f2c
motoyama1020 <babywalk18@gmail.com>
2020-12-11 12:48:18 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'q-chat'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:motoyama/q-chat.git'
+set :repo_url,  'git@github.com:motoyama1020/q-chat.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
